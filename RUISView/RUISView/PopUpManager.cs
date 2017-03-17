961cb2f82492d4e2db163da4b3d56bfd6baa0cfe
Joris <d208399@edu.rocwb.nl>
2017-03-17 09:03:13 +0100

    able to create  pop ups and delete them

--- RUISView/RUISView/PopUpManager.cs
+++ RUISView/RUISView/PopUpManager.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace RUISView
 {
-    class PopUpManager
+    public class PopUpManager
     {
+        PopUp m_popUp = new PopUp();
 
-        private string m_nameOfPopUp;
+        public enum PopUps {Options, Statistics, Info}
+
+        PopUpManager()
+        {
+            m_popUp.cnvInfo.Visibility = System.Windows.Visibility.Collapsed;
+            m_popUp.cnvOptions.Visibility = System.Windows.Visibility.Collapsed;
+            m_popUp.cnvStatistics.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        public void SetPopUpActive(PopUps a_popUpName)
+        {
+            switch (a_popUpName)
+            {
+                case PopUps.Options:
+                    m_popUp.cnvOptions.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case PopUps.Statistics:
+                    m_popUp.cnvStatistics.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                case PopUps.Info:
+                    m_popUp.cnvInfo.Visibility = System.Windows.Visibility.Visible;
+                    break;
+                default:
+                    m_popUp.cnvInfo.Visibility = System.Windows.Visibility.Collapsed;
+                    m_popUp.cnvOptions.Visibility = System.Windows.Visibility.Collapsed;
+                    m_popUp.cnvStatistics.Visibility = System.Windows.Visibility.Collapsed;
+                    break;
+            }
+        }
+
+        public void SetPopUpsInactive()
+        {
+            m_popUp.cnvInfo.Visibility = System.Windows.Visibility.Collapsed;
+            m_popUp.cnvOptions.Visibility = System.Windows.Visibility.Collapsed;
+            m_popUp.cnvStatistics.Visibility = System.Windows.Visibility.Collapsed;
+        }
+        
 
 
 
